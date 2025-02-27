c53287ddd11157b97e1d1b375b45313c10090427
manishrajora25 <manishrajora453@gmail.com>
2025-02-27 15:21:07 +0530

    netflix film web

--- script.js
+++ script.js
@@ -1,58 +1,22 @@
-// import { IMAGE_URL, base_url, API_KEY, requests } from "./data.js";
-
-// const originalsDiv = document.querySelector("#NetflixContainer");
-// const trendingDiv = document.querySelector("#trendingContainer");
-// const topRatedDiv = document.querySelector("#topRatedContainer");
-// const actionMoviesDiv = document.querySelector("#ActionMoviesContainer");
-// const comedyMovieDiv = document.querySelector("comedyMovieContainer");
-// const horronMovieDiv = document.querySelector("horronMovieContainer");
-// const documentaries = document.querySelector("documentariesContainer");
+import { IMAGE_URL, base_url, API_KEY, requests  } from "./data.js";
 
 
-// const data = [];
-
-// async function fetchDataFromURL(value) {
-//   const response = await fetch(base_url + value);
-//   const result = await response.json();
-//   console.log(result.results);
-//   return result.results;
-// }
-
-// for (let i in requests) {
-//   data.push(await fetchDataFromURL(requests[i]));
-// }
-
-// console.log(data);
-
-// data.map((arr) => {
- 
-// });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-import { IMAGE_URL, base_url, API_KEY, requests } from "./data.js";
 
 const originalsDiv = document.querySelector("#NetflixContainer");
 const trendingDiv = document.querySelector("#trendingContainer");
 const topRatedDiv = document.querySelector("#topRatedContainer");
 const actionMoviesDiv = document.querySelector("#ActionMoviesContainer");
-// const comedyMovieDiv = document.querySelector("#comedyMovieContainer");
-// const horronMovieDiv = document.querySelector("#horronMovieContainer");
-// const documentariesDiv = document.querySelector("#documentariesContainer");
-const chengeimgDiv = document.querySelector("#chenge_img");
-const dendom_imgDiv = document.querySelector("#dendom_img_click");
+const comedyMovieDiv = document.querySelector("#comedyMovieContainer");
+const horronMovieDiv = document.querySelector("#horronMovieContainer");
+const romanticMoviesDiv = document.querySelector("#romanticMoviesContainer");
+const documentariesDiv = document.querySelector("#documentariesContainer");
+// const chengeimgDiv = document.querySelector("#chenge_img");
+// const dendom_imgDiv = document.querySelector("#rendom_img_click");
+//const randomImageSection = document.querySelector("#randomImageSection");
+
+
+const navbar = document.querySelector(".maim");
+const content = document.querySelector(".cantent");
 
 
 const data = [];
@@ -73,15 +37,15 @@ for (let i in requests) {
 console.log(data);
 
 function createMovieCard(movie, container) {
-  const movieDiv = document.createElement("div");
-  movieDiv.classList.add("movie-card"); 
+  // const movieDiv = document.createElement("div");
+  // movieDiv.classList.add("movie-card"); 
 
   const movieImg = document.createElement("img");
   movieImg.src = IMAGE_URL + movie.poster_path;
   movieImg.alt = movie.title || movie.name;
 
-  movieDiv.appendChild(movieImg);
-  container.appendChild(movieDiv);
+  // movieDiv.appendChild(movieImg);
+  container.appendChild(movieImg);
 }
 
 data.forEach((movies, index) => {
@@ -89,39 +53,29 @@ data.forEach((movies, index) => {
   else if (index === 1) movies.forEach((movie) => createMovieCard(movie, trendingDiv));
   else if (index === 2) movies.forEach((movie) => createMovieCard(movie, topRatedDiv));
   else if (index === 3) movies.forEach((movie) => createMovieCard(movie, actionMoviesDiv));
-  // else if (index === 4) movies.forEach((movie) => createMovieCard(movie, comedyMovieDiv));
-  // else if (index === 5) movies.forEach((movie) => createMovieCard(movie, horronMovieDiv));
-  // else if (index === 6) movies.forEach((movie) => createMovieCard(movie, documentariesDiv));
+  else if (index === 4) movies.forEach((movie) => createMovieCard(movie, comedyMovieDiv));
+  else if (index === 5) movies.forEach((movie) => createMovieCard(movie, horronMovieDiv));
+  else if (index === 6) movies.forEach((movie) => createMovieCard(movie, romanticMoviesDiv));
+  else if (index === 7) movies.forEach((movie) => createMovieCard(movie, documentariesDiv));
 });
 
 
 
+function getRandomImage() {
+        
+  const randomIndex = Math.floor(Math.random() * data[0].length);
+  const randomMovie = data[0][randomIndex];
+  console.log(randomMovie);
 
+  const Name = document.createElement("h1");
+  Name.textContent = randomMovie.name;
 
-// chengeimgDiv.addEventListener("click", () => {
-//   const allMovies = data.flat(); // Flatten the data array to access all movies
-//   const randomMovie = allMovies[Math.floor(Math.random() * allMovies.length)]; // Pick a random movie
+  const overview = document.createElement("p")
+  overview.textContent = randomMovie.overview;
 
-//   // Update image in chengeimgDiv
-//   chengeimgDiv.innerHTML = ""; // Clear previous content
-//   const randomImg = document.createElement("img");
-//   randomImg.src = IMAGE_URL + randomMovie.poster_path;
-//   randomImg.alt = randomMovie.title || randomMovie.name;
-//   randomImg.classList.add("w-full", "h-full", "object-cover", "rounded-md", "shadow-lg");
-//   chengeimgDiv.appendChild(randomImg);
-// });
+  navbar.style.backgroundImage = `url(${IMAGE_URL + randomMovie.poster_path})`;
+  navbar.style.backgroundSize = "cover";
+  navbar.style.backgroundPosition = "center";
 
-// // Initialize the app
-// createMovieCard();
-
-
-
-
-
-
-
-
-
-
-
-
+  content.prepend(Name,overview)
+}
